3abced4ce65ce131c9063bb557169d199b59a43d
SUJITH-REDDY-YENUGU <c.e.o.yenugusujithreddy@gmail.com>
2025-11-02 17:03:08 +0530

    Fix import formatting and add health check endpoint

--- drowsy_backend/urls.py
+++ drowsy_backend/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Drowsiness backend is running.")),
     path('admin/', admin.site.urls),
-    path('api/',include('monitor.urls')),
+    path('api/', include('monitor.urls')),
 ]
